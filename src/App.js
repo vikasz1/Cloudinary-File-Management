f50fbe8fdc58484bef5238f56dd1c0f019914fa3
Ladki Yadav <91754656+alkaz1@users.noreply.github.com>
2023-10-03 08:45:32 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  const uploadImage = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "tutorial");
+    data.append("cloud_name", "dkvfpxzme");
+    console.log(data)
+    fetch("https://api.cloudinary.com/v1_1/dkvfpxzme/image", {
+      method: "post",
+      body: data
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log("hello")
+      setUrl(data.url);
+    })
+    .catch(err => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <div>
+        <input type="file" onChange={(e) => setImage(e.target.files[0])}></input>
+        <button onClick={uploadImage}>Upload</button>
+      </div>
+      <div>
+        <h1>Uploaded image will be displayed here</h1>
+        <img src={url} alt="Uploaded" />
+      </div>
     </div>
   );
 }
